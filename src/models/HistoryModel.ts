c944c566f6f8e4dee2339a79883f00d97d9342c1
vfa-aupmh <124026859+phamminhhaiauu@users.noreply.github.com>
2025-03-15 21:46:52 +0700

    feat: Refactor BeginVisitUdemyCardAnimation and enhance course check handling
    - Removed the timeout effect from BeginVisitUdemyCardAnimation for cleaner code.
    - Introduced requestBackgroundExecuteCheckCourse function to handle course check asynchronously when navigating to Udemy.
    - Updated setCurrentStep action in HistoryModel to trigger course check when the step changes to GO_TO_UDEMY.

--- src/models/HistoryModel.ts
+++ src/models/HistoryModel.ts
@@ -1,4 +1,6 @@
 import { types } from 'mobx-state-tree';
+import { UdemyMessageAction } from '../services/udemy/types';
+import { getStore, IRootStore } from './RootStore';
 
 
 
@@ -8,12 +10,20 @@ export enum HistoryStep {
   PULL_COURSE = 'pull_course',
   GO_TO_UDEMY = 'go_to_udemy',
 }
+
+const requestBackgroundExecuteCheckCourse = async (store: IRootStore) => {
+    await new Promise(resolve => setTimeout(resolve, 3_000));
+    chrome.runtime.sendMessage({ action: UdemyMessageAction.CHECK_COURSE, coupon: store.coupons[0] });
+}
 // Model cho Coupon
 export const HistoryModel = types.model('History', {
   currentStep: types.enumeration('HistoryStep', Object.values(HistoryStep)),
 })
-.actions(self => ({
+    .actions(self => ({
   setCurrentStep(step: HistoryStep) {
+    if (step === HistoryStep.GO_TO_UDEMY) {
+        requestBackgroundExecuteCheckCourse(getStore());
+    }
     self.currentStep = step;
   },
  
